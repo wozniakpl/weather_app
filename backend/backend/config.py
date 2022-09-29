0ba7e565e40f116b3e7592d32fdb24ae5dbac9dc
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2022-09-29 18:53:54 +0200

    Use postgres

--- backend/backend/config.py
+++ backend/backend/config.py
@@ -5,6 +5,10 @@ DEFAULTS = {
     "DEBUG": (bool, True),
     "SECRET_KEY": (str, "secret-key"),
     "OPENWEATHERMAP_API_KEY": (str, "openweathermap-api-key"),
+    "POSTGRES_DB": (str, "weather"),
+    "POSTGRES_USER": (str, "weather-user"),
+    "POSTGRES_PASSWORD": (str, "weather-secret"),
+    "POSTGRES_HOST": (str, "db"),
 }
 
 env = Env(**DEFAULTS)
--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
+waitforit -host=db -port=5432 --timeout 30
 python manage.py migrate
 python manage.py runserver 0.0.0.0:9000
