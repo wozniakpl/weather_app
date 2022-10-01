82a4984426cdf63866e4f836a9838fb096bcc051
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2022-10-01 13:52:40 +0200

    cluster, ecs service

--- infra/tf_files/envs/backend.tf
+++ infra/tf_files/envs/backend.tf
@@ -2,7 +2,7 @@ data "aws_ecr_repository" "ecr_backend_repo" {
   name = "weather-ecr-repo"
 }
 
-resource "aws_ecs_task_definition" "definition" {
+resource "aws_ecs_task_definition" "backend_task_definition" {
     family                  = "weather-backend-task"
     task_role_arn = aws_iam_role.backend_task_role.arn
     execution_role_arn = aws_iam_role.backend_task_execution_role.arn
@@ -25,4 +25,39 @@ resource "aws_ecs_task_definition" "definition" {
             environment = []
         }
     ])
+}
+
+data "aws_ecs_task_definition" "backend_task_definition_name" {
+    task_definition = aws_ecs_task_definition.backend_task_definition.family
+    depends_on = [
+        aws_ecs_task_definition.backend_task_definition
+    ]
+}
+
+resource "aws_security_group" "backend_security_group" {
+    name = "weather-backend-sg"
+    description = "Weather backend security group"
+    vpc_id = aws_vpc.main.id
+}
+
+resource "aws_ecs_cluster" "weather-cluster" {
+  name = "${var.project_name}-${var.env}-ecs-cluster"
+  tags = local.common_tags
+}
+
+
+resource "aws_ecs_service" "backend_service" {
+    name = "weather-backend-service"
+    cluster = aws_ecs_cluster.weather-cluster.id
+    task_definition = data.aws_ecs_task_definition.backend_task_definition_name.arn
+    desired_count = 1
+    launch_type = "FARGATE"
+    network_configuration {
+        subnets = aws_subnet.main.*.id
+        security_groups = [aws_security_group.backend_security_group.id]
+        assign_public_ip = true
+    }
+    depends_on = [
+        aws_ecs_task_definition.backend_task_definition
+    ]
 }
